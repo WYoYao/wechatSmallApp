0aa8d1cdf663b87b943ba05d1b24b5a30f0be0fc
leo <leosmilence@163.com>
2017-01-10 18:31:24 +0800

    123

---
+++ pages/hotel/index/index.js
@@ -0,0 +1,71 @@
+//index.js
+//获取应用实例
+var app = getApp();
+
+
+Page({
+  data: {
+    text: "This is page data."
+  },
+  onLoad: function(options) {
+    // Do some initialize when page load.
+  },
+  onReady: function() {
+    // wx.setNavigationBarTitle("asd");
+    // Do something when page ready.
+  },
+  onShow: function() {
+    // wx.setNavigationBarTitle("asd");
+    // Do something when page show.
+  },
+  onHide: function() {
+    // Do something when page hide.
+  },
+  onUnload: function() {
+    // Do something when page close.
+  },
+  onPullDownRefresh: function() {
+    // Do something when pull down.
+  },
+  onReachBottom: function() {
+    // Do something when page reach bottom.
+  },
+  onShareAppMessage: function () {
+   // return custom share data when user share.
+  },
+  // Event handler.
+  viewTap: function() {
+    this.setData({
+      text: 'Set some data for updating view.'
+    })
+  },
+  customData: {
+    hi: 'MINA'
+  }
+})
+
+// Page({
+//   data: {
+//     motto: 'Hello World',
+//     userInfo: {}
+//   },
+//   //事件处理函数
+//   bindViewTap: function() {
+//     wx.navigateTo({
+//       url: '../logs/logs'
+//     })
+//   },
+//   onLoad: function () {
+//     console.log('onLoad')
+//     var that = this
+//     //调用应用实例的方法获取全局数据
+//     app.getUserInfo(function(userInfo){
+//       //更新数据
+//       that.setData({
+//         userInfo:userInfo
+//       })
+//     })
+//   }
+// })
+
+
